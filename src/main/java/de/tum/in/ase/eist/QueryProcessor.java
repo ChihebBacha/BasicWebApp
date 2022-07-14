b6ba42aa3742d3fd50625cf003add411817371f6
ge78get <ge78get@mytum.de>
2022-07-14 13:27:20 +0200

    Numbers

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,8 +12,27 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "Chiheb";
-        } else { // TODO extend the programm here
+            return "Chiheb";
+        } else if (query.contains("plus")) { // TODO extend the programm here
+            String[] temp = query.split("is");
+            temp = temp[1].split("plus");
+            temp[0] = temp[0].trim();
+            temp[1] = temp[1].trim();
+            int i1 = Integer.parseInt(temp[0]);
+            int i2 = Integer.parseInt(temp[1]);
+            return String.valueOf(i1 + i2);
+        } else if (query.contains("largest")) {
+            String[] temp = query.split(":");
+            temp = temp[1].split(",");
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < temp.length; i++) {
+                temp[i] = temp[i].trim();
+                if (Integer.parseInt(temp[i]) > max) {
+                    max = Integer.parseInt(temp[i]);
+                }
+            }
+            return String.valueOf(max);
+        } else {
             return "";
         }
     }
